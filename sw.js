fac55d0a66aa966313fbd97776d38ed03c1364a4
Jafar20130315 <windowsnoutbook@gmail.com>
2025-10-25 14:02:30 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,20 @@ const CACHE_NAME = 'cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
+'/soxta-ulanish-sistemasi',
+'/soxta-uyin-bank-kartasi',
+'/soxta-wifi-parol-aniqlovchi',
+'/soxta-joylashuv-aniqlovchi',
+'/soxta-random-yashash-joyi',
+'/soxta-shaxs-aniqlovchi',
+'/avtoraqan-rasm-generatori',
+'/soxta-parol-aniqlovchi',
+'/soxta-balans-aniqlovchi',
+'/oyin-barabani',
+'/bizneschi-oka',
+'/tekst-ovoz',
+'/ovoz-olchovchi',
+
   '/icons/icon-192.png',
   '/icons/icon-512.png'
 ];
