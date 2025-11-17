57a32b29f3459bbf62c0383b58c0ea79e4c1bd91
Jafar20130315 <windowsnoutbook@gmail.com>
2025-11-17 20:57:47 +0500

    Update promokodlar.js

--- promokodlar.js
+++ promokodlar.js
@@ -1,7 +1,3 @@
-<!DOCTYPE HTML>
-  <html>
-<script>
-
 // Agar fayl to'g'ridan-to'g'ri ochilsa, 404 ga yo'naltirish
 if (window.location.pathname.endsWith('promokodlar.js')) {
   window.location.href = '/404.html';
@@ -9,5 +5,3 @@ if (window.location.pathname.endsWith('promokodlar.js')) {
 
 // Promo kodlar ro'yxati
 export const validPromos = ["ZZ478","WOW4444","PRANK-AS1212","AZ2145"];
-  </script>
-    </html>
