1baf6b502c5b4ea4d1533ea4bac8dfd05116eddd
Jafar20130315 <windowsnoutbook@gmail.com>
2025-11-19 13:33:00 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
-const CACHE_NAME = 'cache-v1';
+const CACHE_NAME = 'cache-v2'; // version yangilandi
 const urlsToCache = [
-  '/',
-  '/asosiy',
+'/asosiy', // start_url bilan mos
 '/soxta-ulanish-sistemasi',
 '/soxta-uyin-bank-kartasi',
 '/soxta-wifi-parol-aniqlovchi',
@@ -15,20 +14,30 @@ const urlsToCache = [
 '/bizneschi-oka',
 '/tekst-ovoz',
 '/ovoz-olchovchi',
-
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+'/icons/icon-192.png',
+'/icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+);
+});
+
+self.addEventListener('activate', event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys.filter(key => key !== CACHE_NAME)
+.map(key => caches.delete(key))
+)
+)
+);
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+event.respondWith(
+caches.match(event.request).then(response => response || fetch(event.request))
+);
+})
