3cc8d71fcc50b1fc33eb0115976b569d83d85a72
Jafar20130315 <windowsnoutbook@gmail.com>
2025-12-13 19:05:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,18 +19,34 @@ const urlsToCache = [
   '/icons/icon-512.png'
 ];
 
+// SW install
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
+// SW activate
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
     )
   );
 });
 
+// SW fetch
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  // CSS va JS har doim tarmoqdan yuklansin
+  if (e.request.url.endsWith('.css') || e.request.url.endsWith('.js')) {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+
+  // Boshqa fayllar keshdan yoki tarmoqdan
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
