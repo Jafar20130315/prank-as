ff17a30caae446faf560e780ff261d90425efb98
Jafar20130315 <windowsnoutbook@gmail.com>
2026-01-04 17:23:31 +0300

    Delete promokodlar.js

--- promokodlar.js
+++
@@ -1,7 +0,0 @@
-// Agar fayl to'g'ridan-to'g'ri ochilsa, 404 ga yo'naltirish
-if (window.location.pathname.endsWith('promokodlar.js')) {
-  window.location.href = '/404.html';
-}
-
-// Promo kodlar ro'yxati
-export const validPromos = ["ZZ478","WOW4444","PRANK-AS1212","AZ2145"];
